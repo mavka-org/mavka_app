a52113b9f397678cba587d7b45e8389d0cbf4be4
Danil Makarov <ninjaheister@gmail.com>
2020-04-25 18:13:27 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return StreamProvider<FirebaseUser>.value(
       value: AuthService().user,
       child: MaterialApp(
-        home: Wrapper(),
+        home: Home(),
       ),
     );
   }
